80d58782f7bc329c91e289abc15097c626cc41b1
Vladimir-Maselskiy <65615017+Vladimir-Maselskiy@users.noreply.github.com>
2023-03-15 00:23:00 +0200

    fix: json fetch

--- utils/fetchService.js
+++ utils/fetchService.js
@@ -1,3 +1,8 @@
 export const fetchService = async () => {
-  return await fetch('../data/service.json').then(res => res.json());
+  return await fetch('../data/service.json', {
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+  }).then(res => res.json());
 };
